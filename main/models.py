9bd38cc20dcc57f0aaf3414b2dd77e173adcb2e9
Edmond Christian <edmond.chr@gmail.com>
2024-10-01 10:18:41 +0700

    fixed default.png 5

--- main/models.py
+++ main/models.py
@@ -1,7 +1,8 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.templatetags.static import static
+# from /the_eh_toko/settings import
 # Create your models here.
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -10,4 +11,4 @@ class Product(models.Model):
     description = models.TextField()
     stock = models.IntegerField()
     id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
-    image = models.ImageField(default='default.png', blank=True, upload_to="./media/")
+    image = models.ImageField(blank=True, upload_to="./media/")
