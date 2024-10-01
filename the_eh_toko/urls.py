4bc3e94737115b04cbf097134a3869faee9429fc
Edmond Christian <edmond.chr@gmail.com>
2024-10-01 09:16:56 +0700

    testing image upload

--- the_eh_toko/urls.py
+++ the_eh_toko/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('', include('main.urls')),
 ]
 
-re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
+
